1cb384cf657bd3bb72a5f55600ecc0c9992e3a67
Taha Iqbal <tiqbal10b@gmail.com>
2025-01-12 22:40:33 -0500

    Change title. Deployement changes

--- frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
+++ frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
@@ -16,7 +16,6 @@ const ManageBooks = () => {
             await deleteBook(id).unwrap();
             alert('Book deleted successfully!');
             refetch();
-
         } catch (error) {
             console.error('Failed to delete book:', error.message);
             alert('Failed to delete book. Please try again.');
@@ -27,6 +26,7 @@ const ManageBooks = () => {
     const handleEditClick = (id) => {
         navigate(`dashboard/edit-book/${id}`);
     };
+
   return (
     <section className="py-1 bg-blueGray-50">
     <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
